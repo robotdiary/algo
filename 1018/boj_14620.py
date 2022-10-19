d9325b3fdd3d7c8ac6d2fbcb5b300bc64db5df0b
robotdiary <molihua321.dp@gmail.com>
2022-10-19 19:19:02 +0900

    1019

--- 1018/boj_14620.py
+++ 1018/boj_14620.py
@@ -1,25 +1,26 @@
 # 백준 14620 꽃길 (S)
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
-visited = [(n, n) for _ in range(3)]
-# print(visited)
-answer = [987654321]*3  # 꽃 3개
-for ans in range(3):
-    for i in range(1, n - 1):
-        for j in range(1, n - 1):
-            if (i, j) not in visited[0] and (i, j) not in visited[1]:
-                visit = [(i, j)]
-                price = arr[i][j]
-                for dr, dc in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
-                    if (i + dr, j + dc) not in visited[0] and (i + dr, j + dc) not in visited[1]:
-                        price += arr[i+dr][j+dc]
-                        visit.append((i + dr, j + dc))
-                    else:
-                        break
+
+flower = set()  # 모든 꽃이 있는 장소 저장
+visit = []  # 한 개의 꽃이 있는 장소만 저장
+answer = [987654321]*3
+for count in range(3):
+    for i in range(1, n-1):
+
+        for j in range(1, n-1):
+            price = arr[i][j]
+            visited = [(i, j)]
+            for dr, dc in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+                if (i + dr, j + dc) not in flower:
+                    price += arr[i + dr][j + dc]
+                    visited.append((i + dr, j + dc))
                 else:
-                    if price < answer[ans]:
-                        answer[ans] = price
-                        visited[ans] = visit
-# print(visited)
-# print(answer)
+                    break
+            else:
+                if price < answer[count]:
+                    answer[count] = price
+                    visit = visited
+    for i in visit:
+        flower.add(i)
 print(sum(answer))
