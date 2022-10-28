6e1dc205c4f572766431a8e18bb39842a93dd7f9
robot <molihua321.dp@gmail.com>
2022-10-28 17:42:41 +0900

    1028

--- 1027/swea_14510.py
+++ 1027/swea_14510.py
@@ -3,7 +3,18 @@ T = int(input())
 for tc in range(1, T + 1):
     n = int(input())
     trees = list(map(int, input().split()))
-    print(trees)
-    day = 0
+
+    point = max(trees)
+    day = 1
     pointer = 0
+    # [1] 모든 나무가 커질 때까지 반복
     while pointer <= len(trees):
+        # [2] 전체를 돌면서 물 주기
+        for i in range(pointer, len(trees)):
+            if day % 2:
+                water = 1
+            else:
+                water = 2
+            if point - trees[i] >= water:
+                trees[i] += water
+                day += 1
