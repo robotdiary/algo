6e08528fbd6193ca6cb727521c819a06cfba56ee
robotdiary <molihua321.dp@gmail.com>
2023-02-02 22:56:27 +0900

    0202

--- softeer_1256.py
+++ softeer_1256.py
@@ -1,27 +1,29 @@
 # [인증평가(5차) 기출] 업무 처리
-h, k, r = map(int, input().split())
+h, k, r = map(int, input().split())  # 높이, 업무 수, 날짜
 # 노드의 개수 = 2 ** (h + 1) - 1
-# 말단 직원의 수 = 2 ** h
+high = [2 * x for x in range(h + 1)]
+works = 0
 nodes = [[] for i in range(2 ** (h + 1))]
-for i in range(2 ** h):
-    task = list(map(int, input().split()))
-    for j in task:
-        nodes[2 ** (h + 1) - (2 ** h) + i].append(j)
-
+for i in range(high[-1]):
+    work = list(map(int, input().split()))
+    nodes[2 ** (h + 1) - high[h] + i] += work
+    works += sum(work)
 answer = 0
 day = 2
-while day <= r:
-    for i in range(1, 2 ** (h + 1) - 1 - (2 ** h)):
+while 1 < day <= r:
+    # print(nodes[1], works)
+    if sum(nodes[1]) == works:
+        break
+    for i in range(1, 2 ** (h + 1) - high[h]):
         # 홀수일
         if day % 2:
-            if len(nodes[i * 2]):
+            if nodes[i * 2]:
                 nodes[i].append(nodes[i * 2].pop(0))
+        # 짝수일
         else:
-            if len(nodes[i * 2 + 1]):
+            if nodes[i * 2 + 1]:
                 nodes[i].append(nodes[i * 2 + 1].pop(0))
-        if i == 1:
-            answer += nodes[1].pop(0)
     day += 1
-    # print(nodes)
 
-print(answer)
+    # print(nodes)
+print(sum(nodes[1]))
