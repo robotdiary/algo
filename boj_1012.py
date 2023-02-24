7e78f762091ee1a1fe77ec059a3bc1f857b455b5
robotdiary <molihua321.dp@gmail.com>
2023-02-24 17:03:21 +0900

    0224

--- boj_1012.py
+++ boj_1012.py
@@ -3,14 +3,22 @@ T = int(input())
 for tc in range(T):
     m, n, k = map(int, input().split())  # 가로, 세로, 배추 수
     arr = [[0]*m for _ in range(n)]
-    print(arr)
+    # print(arr)
     for cabbage in range(k):
         r, c = map(int, input().split())
-        arr[r][c] = 1
-    for i in range(m):
-        for j in range(n):
-            if arr[i][j] == 1:
+        arr[c][r] = 1
+    answer = 0
+    for i in range(n):
+        for j in range(m):
+            if arr[i][j]:
+                arr[i][j] = 0
+                answer += 1
                 stack = [(i, j)]
                 while stack:
                     cr, cc = stack.pop()
-                    for nr, nc in [()]
+                    for dr, dc in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
+                        nr, nc = cr + dr, cc + dc
+                        if 0 <= nr < n and 0 <= nc < m and arr[nr][nc]:
+                            stack.append((nr, nc))
+                            arr[nr][nc] = 0
+    print(answer)
