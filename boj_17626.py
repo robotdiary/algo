fe5bbb785af076d5f651063b3622cd859b018cd4
robotdiary <molihua321.dp@gmail.com>
2023-03-11 10:10:13 +0900

    0311

--- boj_17626.py
+++ boj_17626.py
@@ -1,18 +1,17 @@
 # 네네무
-n = int(input())
-first_answer = int(n ** (1/2) // 1)  # 제일 큰 제곱근
-testcase = 5
-for i in range(first_answer):
-    answer = []
-    while n > 1:
-        n -= (first_answer - i) ** 2
-        if len(answer)+1 > testcase:
-            answer.append(first_answer)
-            print("first_answer", first_answer)
-        else:
+n = int(input())  # 수
+first_root = int(n ** (1/2) // 1)  # 제일 큰 제곱근
+ans = [0, 0, 0, 0, 0]
+while first_root:
+    parent = n - (first_root ** 2)
+    answer = [first_root]
+    while parent:
+        if len(answer) >= len(ans):
             break
-    if n:
-        answer.append(1)
-    print(answer)
-
-print(testcase)
+        next_root = int(parent ** (1/2) // 1)
+        parent -= (next_root ** 2)
+        answer.append(next_root)
+    if len(answer) < len(ans):
+        ans = answer
+    first_root -= 1
+print(len(ans))
--- softeer_1016.py
+++ softeer_1016.py
@@ -1,20 +1,29 @@
-n = int(input())
-acup = []  # 대회 점수
-bcup = []  # 점수 정렬
+n = int(input())  # 참가자 수
 
-final = [0]*n  # 점수 합계
-finalcup = []
-rank = []
-for _ in range(n):
-    acup = list(map(int, input().split()))
-    bcup = sorted(acup, reverse=True)
-    ccup = []  # 순위
-    for i in range(n):
-        ccup.append(bcup.index(acup[i]) + 1)
-        final[i] += acup[i]
-    print(*ccup)
+a_cup = list(map(int, input().split()))
+b_cup = list(map(int, input().split()))
+c_cup = list(map(int, input().split()))
 
-finalcup = sorted(final, reverse=True)
+a_rank = sorted(a_cup, reverse=True)
+b_rank = sorted(b_cup, reverse=True)
+c_rank = sorted(c_cup, reverse=True)
+
+final_cup = [0] * n # 점수 합계
 for i in range(n):
-    rank.append(finalcup.index(final[i]) + 1)
-print(*rank)
+    final_cup[i] = a_cup[i]+b_cup[i]+c_cup[i]
+final_rank = sorted(final_cup)
+
+a_answer = []
+b_answer = []
+c_answer = []
+final_answer = []
+for z in range(n):
+    a_answer.append(a_rank.index(a_cup[z]) + 1)
+    b_answer.append(b_rank.index(b_cup[z]) + 1)
+    c_answer.append(c_rank.index(c_cup[z]) + 1)
+    final_answer.append(final_rank.index(final_cup[z]) + 1)
+
+print(*a_answer)
+print(*b_answer)
+print(*c_answer)
+print(*final_answer)
