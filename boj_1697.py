5ae400cae382950dd19c74995e3d649a625518ec
robotdiary <molihua321.dp@gmail.com>
2023-03-22 09:26:15 +0900

    0322

--- boj_1697.py
+++ boj_1697.py
@@ -1,30 +1,10 @@
-# n, k = map(int, input().split())
-# if n != k:
-#     answer = 0
-#     while n != k:
-#         if n + (n // 2) > k:
-#             n += 1
-#         elif n * 2 > k + 1:
-#             n -= 1
-#         else:
-#             n *= 2
-#         answer += 1
-#     print(answer)
-# else:
-#     print(0)
-
-# 메모리 초과
+# 숨바꼭질
 from collections import deque
 n, k = map(int, input().split())
 if n != k:
     stack = deque()
-    end = -1
-    if n > k:
-        stack.append(k)
-        end = n
-    else:
-        stack.append(n)
-        end = k
+    stack.append(n)
+    end = k
     #bfs
     answer = 0
     visited = {stack[0]}
@@ -35,15 +15,20 @@ if n != k:
                 answer += 1
                 stack = 0
                 break
-            if start + 1 not in visited and start + 1 < 100000:
-                stack.append(start + 1)
-                visited.add(start + 1)
-            if start - 1 not in visited:
-                stack.append(start - 1)
-                visited.add(start - 1)
-            if start < end and start * 2 < 100000 and start * 2 not in visited:
-                stack.append(start * 2)
-                visited.add(start * 2)
+            for next in [start + 1, start - 1, start * 2]:
+                if next not in visited and 0 <= next <= 100000:
+                    stack.append(next)
+                    visited.add(next)
+            #
+            # if start + 1 not in visited and start + 1 < 100000:
+            #     stack.append(start + 1)
+            #     visited.add(start + 1)
+            # if start - 1 not in visited and start - 1 >= 0:
+            #     stack.append(start - 1)
+            #     visited.add(start - 1)
+            # if start < end and start * 2 < 100000 and start * 2 not in visited:
+            #     stack.append(start * 2)
+            #     visited.add(start * 2)
         else:
             answer += 1
     print(answer)
